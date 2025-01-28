1ca6514dd2d459e9cb521955cd3bd011aa3fc60a
Austin Karren <austin.t.karren@gmail.com>
2025-01-27 21:26:41 -0700

    add db auth token

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,8 +5,11 @@ import { env } from "@gitfleet/env";
 export default {
   schema: "./src/server/db/schema.ts",
   dialect: "sqlite",
+  // comment out turso driver to use a local db.sqlite file
+  driver: "turso",
   dbCredentials: {
     url: env.DATABASE_URL,
+    authToken: env.DATABASE_AUTH_TOKEN,
   },
   tablesFilter: ["gitfleet_*"],
 } satisfies Config;
